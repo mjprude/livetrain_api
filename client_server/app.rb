f3da5722bbdb0791f18067f61f28d5c1d6ca74a2
mjprude <michaelprude@gmail.com>
2014-12-18 16:46:01 -0500

    copy contents of mta_realtime repo and remove everything but the backend server

--- client_server/app.rb
+++
@@ -1,6 +0,0 @@
-require 'bundler'
-Bundler.require
-
-get '/' do
-  erb :index
-end
---
+++ models/station.rb
@@ -0,0 +1,6 @@
+class Station < ActiveRecord::Base
+  # has_many :stops
+  # has_many :lines, through: :stops
+
+
+end
