b244fc6e64af3b0f8482ef601d57c73335d12952
mjprude <michaelprude@gmail.com>
2014-12-27 16:42:35 -0800

    db_helper method working

--- helpers/db_helper.rb
+++ helpers/db_helper.rb
@@ -58,41 +58,41 @@ module DBHelper
         trip2Complete = (stop2 == nil)
         stop3 = trip[3]
         trip3Complete = (stop3 == nil)
+
+        route_obj = {
+          trip_id: 't' + stop1['mta_trip_id'].gsub('.', '_'),
+          route: stop1['route'],
+          direction: stop1['direction'],
+          updated: stop1['mta_timestamp'],
+
+          lastStop: last_stop['stop_id'],
+          lastDeparture: last_stop['departure_time'],
+
+          stop1: stop1['stop_id'],
+          path1: Shapes.get_path(stop1['route'], last_stop['stop_id'], stop1['stop_id']),
+          arrival1: stop1['arrival_time'],
+          departure1: stop1['departure_time'],
+
+          trip1Complete: false,
+          trip2Complete: trip2Complete,
+          trip3Complete: trip3Complete
+        }
+
+        if stop2
+          route_obj[:stop2] = stop2['stop_id']
+          route_obj[:path2] = Shapes.get_path(stop1['route'], stop1['stop_id'], stop2['stop_id'])
+          route_obj[:arrival2] = stop2['arrival_time']
+          route_obj[:departure2] = stop2['departure_time']
+        end
+
+        if stop3
+          route_obj[:stop3] = stop3['stop_id']
+          route_obj[:path3] = Shapes.get_path(stop1['route'], stop2['stop_id'], stop3['stop_id'])
+          route_obj[:arrival3] = stop3['arrival_time']
+          route_obj[:departure3] = stop3['departure_time']
+        end
+        json_ary << route_obj
       end
-
-      route_obj = {
-        trip_id: 't' + stop1['mta_trip_id'].gsub('.', '_'),
-        route: stop1['route'],
-        direction: stop1['direction'],
-        updated: stop1['mta_timestamp'],
-
-        lastStop: last_stop['stop_id'],
-        lastDeparture: last_stop['departure_time'],
-
-        stop1: stop1['stop_id'],
-        path1: Shapes.get_path(stop1['route'], last_stop['stop_id'], stop1['stop_id']),
-        arrival1: stop1['arrival_time'],
-        departure1: stop1['departure_time'],
-
-        trip1Complete: false,
-        trip2Complete: trip2Complete,
-        trip3Complete: trip3Complete
-      }
-
-      if stop2
-        route_obj[:stop2] = stop2['stop_id']
-        route_obj[:path2] = Shapes.get_path(stop1['route'], stop1['stop_id'], stop2['stop_id'])
-        route_obj[:arrival2] = stop2['arrival_time']
-        route_obj[:departure2] = stop2['departure_time']
-      end
-
-      if stop3
-        route_obj[:stop3] = stop3['stop_id']
-        route_obj[:path3] = Shapes.get_path(stop1['route'], stop2['stop_id'], stop3['stop_id'])
-        route_obj[:arrival3] = stop3['arrival_time']
-        route_obj[:departure3] = stop3['departure_time']
-      end
-      json_ary << route_obj
     end
 
 
