b5cbd66f948aefa5d5fa564d093d26aaafd6a3f5
Ted Mahoney <ted.mahoney22@gmail.com>
2015-01-28 00:39:29 -0500

    sends route and destination in json response to GET api/train

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,8 @@ class Trip < ActiveRecord::Base
   has_many :stops, dependent: :destroy
 
   def self.send_info(train_id)
+    @destinations = JSON.parse(File.read(Rails.root + 'app/assets/static_MTA/uniq_route_shapes.json'))
+
     train = Trip.find(train_id)
 
     stops = []
@@ -9,14 +11,18 @@ class Trip < ActiveRecord::Base
       stops << {
         mta_stop_id: stop.stop_id[0..-2],
         arrival_time: stop.arrival_time,
-        departure_time: stop.departure_time
+        departure_time: stop.departure_time,
       }
     end
 
+    time, shape_id = train.mta_trip_id.split('_')
+
     return_info = {
       trip_id: train.id,
+      route: train.route,
       direction: train.direction,
-      stops: stops
+      stops: stops,
+      destination: @destinations[shape_id[0..5]] == nil ? nil : @destinations[shape_id[0..5]].split(' ').map{|word| word.capitalize}.join(' ')
     }
   end
 
